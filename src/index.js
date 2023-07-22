aa828613aaa4f6999c553becf1af16578194964d
Fatma Hussein <fartunhussein346@gmail.com>
2023-07-22 15:11:54 +0300

    added add, edit and delete funtionalities

--- src/index.js
+++ src/index.js
@@ -1,45 +1,116 @@
+/* eslint-disable no-use-before-define */
 import './styles.css';
 
-const tasksArray = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Complete To Do List project',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Complete Microverse exercises',
-    completed: false,
-    index: 1,
-  },
-];
+let tasksArray = [];
 
-tasksArray.sort((a, b) => a.index - b.index);
+function addTask() {
+  const input = document.querySelector('#task');
+  const value = input.value.trim();
+  if (value !== '') {
+    const task = {
+      description: value,
+      completed: false,
+      index: tasksArray.length + 1,
+    };
+    tasksArray.push(task);
+    input.value = '';
+    addtoLocalStorage();
+  }
+  displayTasks();
+}
 
-tasksArray.forEach((task) => {
-  const element = document.querySelector('tbody');
-  const tr2 = document.createElement('tr');
-  const td2 = document.createElement('td');
-  const input = document.createElement('input');
-  input.type = 'checkbox';
-  td2.appendChild(input);
-  tr2.appendChild(td2);
-  const td3 = document.createElement('td');
-  td3.classList.add('align');
-  const label = document.createElement('label');
-  label.textContent = task.description;
-  td3.appendChild(label);
-  const td4 = document.createElement('td');
-  td4.innerHTML = `<i class='fa-solid fa-ellipsis-vertical'>
-  </i>`;
-  tr2.appendChild(td3);
-  tr2.appendChild(td4);
-  element.appendChild(tr2);
-  const table = document.querySelector('table');
-  table.appendChild(element);
-  document.body.appendChild(table);
+function updateTaskIndexes() {
+  tasksArray.forEach((task, index) => {
+    task.index = index + 1;
+  });
+}
+
+function displayTasks() {
+  tasksArray.sort((a, b) => a.index - b.index);
+  const listItem = document.querySelector('.list-items');
+  listItem.innerHTML = '';
+
+  tasksArray.forEach((item) => {
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = item.completed;
+    checkbox.addEventListener('change', () => {
+      item.completed = checkbox.checked;
+      updateTaskIndexes();
+      addtoLocalStorage();
+      displayTasks();
+    });
+
+    const value = document.createElement('label');
+    value.textContent = item.description;
+    value.contentEditable = true;
+    value.addEventListener('input', () => {
+      item.description = value.textContent.trim();
+      addtoLocalStorage();
+    });
+
+    const deleteItem = document.createElement('button');
+    deleteItem.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteItem.addEventListener('click', () => {
+      tasksArray = tasksArray.filter((n) => n !== item);
+      updateTaskIndexes();
+      addtoLocalStorage();
+      displayTasks();
+    });
+
+    const line = document.createElement('hr');
+    li.appendChild(checkbox);
+    li.appendChild(value);
+    li.appendChild(deleteItem);
+    listItem.appendChild(li);
+    listItem.appendChild(line);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadtoLocalStorage();
+  const form = document.querySelector('#form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTask();
+  });
+
+  const add = document.querySelector('#enter');
+  add.addEventListener('click', () => {
+    addTask();
+  });
+
+  const input = document.querySelector('#task');
+  input.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      addTask();
+    }
+  });
+
+  displayTasks();
 });
+
+function deleteTask() {
+  const clear = document.querySelector('.clear-completed');
+  clear.addEventListener('click', () => {
+    tasksArray = tasksArray.filter((n) => !n.completed);
+    updateTaskIndexes();
+    addtoLocalStorage();
+    displayTasks();
+  });
+}
+deleteTask();
+
+function addtoLocalStorage() {
+  localStorage.setItem('data', JSON.stringify(tasksArray));
+}
+
+function loadtoLocalStorage() {
+  const dataValue = localStorage.getItem('data');
+  if (dataValue) {
+    tasksArray = JSON.parse(dataValue);
+    displayTasks();
+  }
+}
